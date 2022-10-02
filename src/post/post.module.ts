066705b02b6592e4efd035a1ec9f3ec215d46ba7
tuan-le-dss <minhtuan.le@dssolution.jp>
2022-10-03 00:05:40 +0700

    connect Mongodb

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PostController } from './controllers/post.controller';
+import { PostSchema } from './models/post.model';
+import { PostRepository } from './repositories/post.repository';
 import { PostService } from './services/post.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Post',
+        schema: PostSchema,
+      },
+    ]),
+  ],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, PostRepository],
 })
 export class PostModule {}
